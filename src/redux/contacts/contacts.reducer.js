431b89a43e15f07381197e5cb75dc162985b6f07
Midomik <krastikrabbs9234@gmail.com>
2023-11-27 14:04:06 +0200

    comm 4

--- src/redux/contacts/contacts.reducer.js
+++ src/redux/contacts/contacts.reducer.js
@@ -36,7 +36,6 @@ export const addContact = createAsyncThunk(
   'contacts/add',
   async (contactInfo, thunkApi) => {
     try {
-      console.log(contactInfo);
       const { data } = await axios.post('contacts', contactInfo);
 
       return data;
@@ -49,8 +48,6 @@ export const addContact = createAsyncThunk(
 export const removeContact = createAsyncThunk(
   'contacts/delete',
   async (contact_id, thunkApi) => {
-    console.log(contact_id);
-
     try {
       await axios.delete(`contacts/${contact_id}`);
       return contact_id;
@@ -64,7 +61,6 @@ export const addToFavorite = createAsyncThunk(
   'contacts/add_to_favorite',
   async (contact_info, thunkApi) => {
     try {
-      console.log('id', ' ', contact_info);
       await axios.put(`contacts/${contact_info.id}`, {
         isFavorite: !contact_info.isFavorite,
       });
@@ -74,48 +70,6 @@ export const addToFavorite = createAsyncThunk(
     }
   }
 );
-
-// export const addToRecentlyDailed = createAsyncThunk(
-//   'recentlyDailed/add_to_recently_dailed',
-//   async (contact_info, thunkApi) => {
-//     try {
-//       await axios.post(`recently_dailed`, {
-//         recectlyItemInfo: contact_info,
-//         recentlyItemTime: `${
-//           new Date().getUTCHours() + 2
-//         }:${new Date().getUTCMinutes()}`,
-//       });
-
-//       return contact_info;
-//     } catch (error) {
-//       return thunkApi.rejectWithValue(error.message);
-//     }
-//   }
-// );
-
-// export const removeFromRecentlyDailed = createAsyncThunk(
-//   'recentlyDailed/remove_from_recently_dailed',
-//   async (contact_id, thunkApi) => {
-//     try {
-//       await axios.delete(`recently_dailed/${contact_id}`);
-//       return contact_id;
-//     } catch (error) {
-//       return thunkApi.rejectWithValue(error.message);
-//     }
-//   }
-// );
-
-// export const getRecentlyDailed = createAsyncThunk(
-//   'recentlyDailed/get_recently_dailed',
-//   async (_, thunkApi) => {
-//     try {
-//       const { data } = await axios.get(`recently_dailed`);
-//       return data;
-//     } catch (error) {
-//       return thunkApi.rejectWithValue(error.message);
-//     }
-//   }
-// );
 
 const ContactsSlice = createSlice({
   name: 'contacts',
@@ -189,55 +143,6 @@ const ContactsSlice = createSlice({
   },
 });
 
-// const RecentlyDailedSlice = createSlice({
-//   name: 'recentlyDailed',
-
-//   initialState: recentlyDailedInitialState,
-//   reducers: {
-//     //save local history of calls
-//     addToLocalRecentlyArray(state, { payload }) {
-//       state.recentlyArray.push(payload);
-//     },
-//     filterRecentlyDailedList(state, { payload }) {
-//       state.filter = payload;
-//     },
-//   },
-//   extraReducers: builder => {
-//     builder
-//       .addCase(getRecentlyDailed.pending, (state, { payload }) => {
-//         state.recentlyDailed.isLoading = true;
-//         state.recentlyDailed.error = null;
-//       })
-//       .addCase(getRecentlyDailed.fulfilled, (state, { payload }) => {
-//         state.recentlyDailed.isLoading = false;
-//         state.recentlyDailed.items = payload;
-//       })
-//       .addCase(getRecentlyDailed.rejected, (state, { payload }) => {
-//         state.recentlyDailed.isLoading = false;
-//         state.recentlyDailed.error = payload;
-//       });
-
-//     builder
-//       .addCase(removeFromRecentlyDailed.pending, (state, { payload }) => {
-//         state.recentlyDailed.isLoading = true;
-//         state.recentlyDailed.error = null;
-//       })
-//       .addCase(removeFromRecentlyDailed.fulfilled, (state, { payload }) => {
-//         state.recentlyDailed.isLoading = false;
-//         state.recentlyDailed.items = state.recentlyDailed.items.filter(
-//           item => item.id !== payload
-//         );
-//       })
-//       .addCase(removeFromRecentlyDailed.rejected, (state, { payload }) => {
-//         state.recentlyDailed.isLoading = false;
-//         state.recentlyDailed.error = payload;
-//       });
-//   },
-// });
 export const { filterContactList } = ContactsSlice.actions;
 
-// export const { filterRecentlyDailedList } = RecentlyDailedSlice.actions;
-
 export const contactsReducer = ContactsSlice.reducer;
-
-// export const recentlyDailedReducer = RecentlyDailedSlice.reducer;
